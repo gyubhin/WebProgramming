4634b4de7d07d5db2391130bc838d71ffbf81ce8
김규빈 <113112349+gyubhin@users.noreply.github.com>
2024-06-14 23:15:14 +0900

    Add files via upload

---
+++ 6.14 마지막 수정/bldg.js
@@ -0,0 +1,222 @@
+  const button_A11 = document.querySelector(".button_A11");
+  const dialog_A11 = document.querySelector(".dialog_A11");
+  const button_A12 = document.querySelector(".button_A12");
+  const dialog_A12 = document.querySelector(".dialog_A12");
+  const button_A13 = document.querySelector(".button_A13");
+  const dialog_A13 = document.querySelector(".dialog_A13");
+  const button_A15 = document.querySelector(".button_A15");
+  const dialog_A15 = document.querySelector(".dialog_A15");
+  const button_A21 = document.querySelector(".button_A21");
+  const dialog_A21 = document.querySelector(".dialog_A21");
+  const button_A22 = document.querySelector(".button_A22");
+  const dialog_A22 = document.querySelector(".dialog_A22");
+  const button_A23 = document.querySelector(".button_A23");
+  const dialog_A23 = document.querySelector(".dialog_A23");
+
+  const button_B11 = document.querySelector(".button_B11");
+  const dialog_B11 = document.querySelector(".dialog_B11");
+  const button_B12 = document.querySelector(".button_B12");
+  const dialog_B12 = document.querySelector(".dialog_B12");
+  const button_B13 = document.querySelector(".button_B13");
+  const dialog_B13 = document.querySelector(".dialog_B13");
+  const button_B14 = document.querySelector(".button_B14");
+  const dialog_B14 = document.querySelector(".dialog_B14");
+  const button_B15 = document.querySelector(".button_B15");
+  const dialog_B15 = document.querySelector(".dialog_B15");
+  const button_B16 = document.querySelector(".button_B16");
+  const dialog_B16 = document.querySelector(".dialog_B16");
+
+  const button_C11 = document.querySelector(".button_C11");
+  const dialog_C11 = document.querySelector(".dialog_C11");
+  const button_C12 = document.querySelector(".button_C12");
+  const dialog_C12 = document.querySelector(".dialog_C12");
+  const button_C13 = document.querySelector(".button_C13");
+  const dialog_C13 = document.querySelector(".dialog_C13");
+  const button_C14 = document.querySelector(".button_C14");
+  const dialog_C14 = document.querySelector(".dialog_C14");
+  const button_C21 = document.querySelector(".button_C21");
+  const dialog_C21 = document.querySelector(".dialog_C21");
+  const button_C22 = document.querySelector(".button_C22");
+  const dialog_C22 = document.querySelector(".dialog_C22");
+  const button_C23 = document.querySelector(".button_C23");
+  const dialog_C23 = document.querySelector(".dialog_C23");
+  const button_C24 = document.querySelector(".button_C24");
+  const dialog_C24 = document.querySelector(".dialog_C24");
+  const button_C25 = document.querySelector(".button_C25");
+  const dialog_C25 = document.querySelector(".dialog_C25");
+  const button_C26 = document.querySelector(".button_C26");
+  const dialog_C26 = document.querySelector(".dialog_C26");
+  const button_C27 = document.querySelector(".button_C27");
+  const dialog_C27 = document.querySelector(".dialog_C27");
+  const button_C28 = document.querySelector(".button_C28");
+  const dialog_C28 = document.querySelector(".dialog_C28");
+
+  const button_D12 = document.querySelector(".button_D12");
+  const dialog_D12 = document.querySelector(".dialog_D12");
+  const button_D13 = document.querySelector(".button_D13");
+  const dialog_D13 = document.querySelector(".dialog_D13");
+  const button_D14 = document.querySelector(".button_D14");
+  const dialog_D14 = document.querySelector(".dialog_D14");
+  const button_D21 = document.querySelector(".button_D21");
+  const dialog_D21 = document.querySelector(".dialog_D21");
+  const button_D22 = document.querySelector(".button_D22");
+  const dialog_D22 = document.querySelector(".dialog_D22");
+  const button_D23 = document.querySelector(".button_D23");
+  const dialog_D23 = document.querySelector(".dialog_D23");
+  const button_D24 = document.querySelector(".button_D24");
+  const dialog_D24 = document.querySelector(".dialog_D24");
+
+  const button_E11 = document.querySelector(".button_E11");
+  const dialog_E11 = document.querySelector(".dialog_E11");
+  const button_E12 = document.querySelector(".button_E12");
+  const dialog_E12 = document.querySelector(".dialog_E12");
+  const button_E13 = document.querySelector(".button_E13");
+  const dialog_E13 = document.querySelector(".dialog_E13");
+  const button_E14 = document.querySelector(".button_E14");
+  const dialog_E14 = document.querySelector(".dialog_E14");
+  const button_E21 = document.querySelector(".button_E21");
+  const dialog_E21 = document.querySelector(".dialog_E21");
+  const button_E22 = document.querySelector(".button_E22");
+  const dialog_E22 = document.querySelector(".dialog_E22");
+  const button_E29 = document.querySelector(".button_E29");
+  const dialog_E29 = document.querySelector(".dialog_E29");
+
+  button_A11.addEventListener("click", () => { 
+    dialog_A11.showModal()
+  })
+  button_A12.addEventListener("click", () => {
+    dialog_A12.showModal()
+  })
+  button_A13.addEventListener("click", () => {
+    dialog_A13.showModal()
+  })
+  button_A15.addEventListener("click", () => {
+    dialog_A15.showModal()
+  })
+  button_A21.addEventListener("click", () => {
+    dialog_A21.showModal()
+  })
+  button_A22.addEventListener("click", () => {
+    dialog_A22.showModal()
+  })
+  button_A23.addEventListener("click", () => {
+    dialog_A23.showModal()
+  })
+
+  button_B11.addEventListener("click", () => { 
+    dialog_B11.showModal()
+  })
+  button_B12.addEventListener("click", () => {
+    dialog_B12.showModal()
+  })
+  button_B13.addEventListener("click", () => {
+    dialog_B13.showModal()
+  })
+  button_B14.addEventListener("click", () => {
+    dialog_B14.showModal()
+  })
+  button_B15.addEventListener("click", () => {
+    dialog_B15.showModal()
+  })
+  button_B16.addEventListener("click", () => {
+    dialog_B16.showModal()
+  })
+
+  button_C11.addEventListener("click", () => { 
+    dialog_C11.showModal()
+  })
+  button_C12.addEventListener("click", () => {
+    dialog_C12.showModal()
+  })
+  button_C13.addEventListener("click", () => {
+    dialog_C13.showModal()
+  })
+  button_C14.addEventListener("click", () => {
+    dialog_C14.showModal()
+  })
+  button_C21.addEventListener("click", () => {
+    dialog_C21.showModal()
+  })
+  button_C22.addEventListener("click", () => {
+    dialog_C22.showModal()
+  })
+  button_C23.addEventListener("click", () => {
+    dialog_C23.showModal()
+  })
+  button_C24.addEventListener("click", () => {
+    dialog_C24.showModal()
+  })
+  button_C25.addEventListener("click", () => {
+    dialog_C25.showModal()
+  })
+  button_C26.addEventListener("click", () => {
+    dialog_C26.showModal()
+  })
+  button_C27.addEventListener("click", () => {
+    dialog_C27.showModal()
+  })
+  button_C28.addEventListener("click", () => {
+    dialog_C28.showModal()
+  })
+
+  button_D12.addEventListener("click", () => {
+    dialog_D12.showModal()
+  })
+  button_D13.addEventListener("click", () => {
+    dialog_D13.showModal()
+  })
+  button_D14.addEventListener("click", () => {
+    dialog_D14.showModal()
+  })
+  button_D21.addEventListener("click", () => {
+    dialog_D21.showModal()
+  })
+  button_D22.addEventListener("click", () => {
+    dialog_D22.showModal()
+  })
+  button_D23.addEventListener("click", () => {
+    dialog_D23.showModal()
+  })
+  button_D24.addEventListener("click", () => { 
+    dialog_D24.showModal()
+  })
+
+  button_E11.addEventListener("click", () => { 
+    dialog_E11.showModal()
+  })
+  button_E12.addEventListener("click", () => {
+    dialog_E12.showModal()
+  })
+  button_E13.addEventListener("click", () => {
+    dialog_E13.showModal()
+  })
+  button_E14.addEventListener("click", () => {
+    dialog_E14.showModal()
+  })
+  button_E21.addEventListener("click", () => {
+    dialog_E21.showModal()
+  })
+  button_E22.addEventListener("click", () => {
+    dialog_E22.showModal()
+  })
+  button_E29.addEventListener("click", () => {
+    dialog_E29.showModal()
+  })
+  var target = document.querySelectorAll('.btn_open');
+  var btnPopClose = document.querySelectorAll('.pop_wrap .btn_close');
+  var targetID;
+
+  // 팝업 열기
+  for(var i = 0; i < target.length; i++){
+    target[i].addEventListener('click', function(){
+      targetID = this.getAttribute('href');
+      document.querySelector(targetID).style.display = 'block';
+    });
+  }
+
+  // 팝업 닫기
+  for(var j = 0; j < target.length; j++){
+    btnPopClose[j].addEventListener('click', function(){
+      this.parentNode.parentNode.style.display = 'none';
+    });
+  }
